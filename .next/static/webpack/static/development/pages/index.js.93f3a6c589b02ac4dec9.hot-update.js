5ca1a10b5e5e1d7da8d980efef378d7ebe2af665
Amjad Masad <amjad.masad@gmail.com>
2020-03-03 22:34:09 +0000

    Initial commit

---
+++ .next/static/webpack/static/development/pages/index.js.93f3a6c589b02ac4dec9.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/runner/FortunateTallCodec/pages/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+function HomePage() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "js!");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (HomePage);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.93f3a6c589b02ac4dec9.hot-update.js.map
---
+++ .next/static/webpack/static/development/pages/index.js.d81abb259f912931e1a7.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/runner/FortunateTallCodec/pages/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+function HomePage() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "henlo js!");
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (HomePage);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.d81abb259f912931e1a7.hot-update.js.map
